84e29174222b7c0219ea8645b1ac2d4778119feb
Stanislav Frolov <frolov@mrtknecht1.mrt.uni-karlsruhe.de>
2017-06-19 15:31:56 +0200

    relative path

--- run_test.py
+++ run_test.py
@@ -2,8 +2,8 @@ from darkflow.cli import cliHandler
 
 
 #command = 'flow --imgdir /Users/sfrolov/master-thesis/img/boy --model cfg/tiny-yolo-voc.cfg --load bin/tiny-yolo-voc.weights'
-#command = 'flow --model cfg/tiny-yolo-voc.cfg --train --dataset ./VOCdevkit/VOC2007/JPEGImages --annotation ./VOCdevkit/VOC2007/Annotations'
-command = 'flow --model cfg/motnet.cfg --load bin/yolo.weights --train --trainer adam'
+command = './flow --model cfg/tiny-yolo-voc.cfg --train --gpu 1.0 --dataset ./VOCdevkit/VOC2007/JPEGImages --annotation ./VOCdevkit/VOC2007/Annotations'
+#command = 'flow --model cfg/motnet.cfg --load bin/yolo.weights --train --trainer adam'
 
 command = command.split()
 
