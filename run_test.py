d563be7eae2a0774042a61112d48771c0e249188
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2017-06-26 13:57:24 +0200

    Allow growth

--- run_test.py
+++ run_test.py
@@ -6,7 +6,7 @@ from darkflow.cli import cliHandler
 
 
 #command = './flow --model cfg/tiny-yolo-voc.cfg --train --gpu 1.0 --dataset ./VOCdevkit/VOC2007/JPEGImages --annotation ./VOCdevkit/VOC2007/Annotations'
-command = './flow --model cfg/motnet.cfg --train --trainer adam --gpu 0.8'
+command = './flow --model cfg/motnet.cfg --train --trainer adam --gpu 0.9'
 
 command = command.split()
 
