3608b8974519e9a006e50f2c3a05a91dfe6034fb
Stanislav Frolov <frolov@mrtknecht1.mrt.uni-karlsruhe.de>
2017-08-12 13:09:15 +0200

    Fix paths

--- flow_all_img.py
+++ flow_all_img.py
@@ -1,7 +1,7 @@
 from darkflow.cli import cliHandler
 
 
-list = ['/home/frolov/U/MOTC/MOT17/train/MOT17-02-FRCNN/img1',
+all_dirs = ['/home/frolov/U/MOTC/MOT17/train/MOT17-02-FRCNN/img1',
         '/home/frolov/U/MOTC/MOT17/train/MOT17-04-FRCNN/img1',
         '/home/frolov/U/MOTC/MOT17/train/MOT17-05-FRCNN/img1',
         '/home/frolov/U/MOTC/MOT17/train/MOT17-09-FRCNN/img1',
@@ -16,8 +16,8 @@ list = ['/home/frolov/U/MOTC/MOT17/train/MOT17-02-FRCNN/img1',
         '/home/frolov/U/MOTC/MOT17/test/MOT17-12-FRCNN/img1',
         '/home/frolov/U/MOTC/MOT17/test/MOT17-14-FRCNN/img1',]
         
-for dir in list:
+for path in all_dirs:
     print("Now flowing " + dir)
-    command = './flow --imgdir ' + dir + ' --model cfg/yolo.cfg --load bin/yolo.weights'
+    command = './flow --imgdir ' + path + ' --model cfg/yolo.cfg --load bin/yolo.weights'
     command = command.split()
     cliHandler(command)
