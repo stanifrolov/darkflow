bc2bd41ca33dab434087dc4b1c97025c8139bc90
Stanislav Frolov <frolov@mrtknecht1.mrt.uni-karlsruhe.de>
2017-08-12 10:54:23 +0200

    Added read from local storage and Added getLatestCheckpoint

--- run_test.py
+++ run_test.py
@@ -1,4 +1,19 @@
 from darkflow.cli import cliHandler
+import os
+
+def getLatestCheckpoint():
+    imgdir = os.path.join('/home/frolov/U/darkflow/ckpt')  # gibt path von Checkpoints an
+    file_list = []
+    for dirpath, dirnames, files in os.walk(imgdir):   # os.walk gibt Pfad an, alle Ordner im Ordner und alle Dateien im Ordner.
+            for f in files:
+                if f.endswith('.index'):
+                    filename = os.path.join(dirpath, f)
+                    file_list.append(filename)
+    file_list.sort()
+    file = file_list[len(file_list) - 1]
+    file = file.split('.index')
+    file = file[0].split('-')
+    return file[len(file) - 1]
 
 """
 Test data
@@ -20,12 +35,13 @@ Working configs
 Training Commands
 """
 #command = './flow --model cfg/full_motnet.cfg --load bin/yolo.weights --train --gpu 0.9 --seq_length 10'
-command = './flow --model cfg/tiny_motnet.cfg --load bin/tiny-yolo-voc.weights --train --gpu 0.9 --seq_length 3'
+#command = './flow --model cfg/tiny_motnet.cfg --load bin/tiny-yolo-voc.weights --train --gpu 0.9 --seq_length 3'
 
 """
 Training on Machine
 """
-#command = './flow --model cfg/full_motnet.cfg --load 17000 --train --gpu 0.9'
+last_ckpt = getLatestCheckpoint()
+command = './flow --model cfg/full_motnet.cfg --load 33000 --train --gpu 0.9' + ' --load ' + last_ckpt
 
 """
 Run the command
