afe434c43b0d8e20651270b9d41b7113a3cf3777
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2017-08-22 14:21:44 +0200

    seq length 5

--- run_test.py
+++ run_test.py
@@ -25,7 +25,7 @@ Training Commands
 """
 Training on Machine
 """
-command = './flow --model cfg/full_motnet.cfg --train --gpu 0.9 --batch 1 --seq_length 1'
+command = './flow --model cfg/full_motnet.cfg --train --gpu 0.9 --batch 1 --seq_length 5'
 
 """
 Run the command
