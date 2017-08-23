daf2ba468ede5eb21b95fe009818acb5127305c6
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2017-08-23 11:43:12 +0200

    Flow all finetuned

--- motc_utils/flow_all_img.py
+++ motc_utils/flow_all_img.py
@@ -2,22 +2,22 @@ from darkflow.cli import cliHandler
 
 
 all_dirs = ['/home/frolov/U/MOTC/MOT17/train/MOT17-02-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/train/MOT17-04-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/train/MOT17-05-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/train/MOT17-09-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/train/MOT17-10-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/train/MOT17-11-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/train/MOT17-13-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/test/MOT17-01-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/test/MOT17-03-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/test/MOT17-06-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/test/MOT17-07-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/test/MOT17-08-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/test/MOT17-12-FRCNN/img1',
-        '/home/frolov/U/MOTC/MOT17/test/MOT17-14-FRCNN/img1',]
+            '/home/frolov/U/MOTC/MOT17/train/MOT17-04-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/train/MOT17-05-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/train/MOT17-09-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/train/MOT17-10-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/train/MOT17-11-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/train/MOT17-13-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/test/MOT17-01-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/test/MOT17-03-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/test/MOT17-06-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/test/MOT17-07-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/test/MOT17-08-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/test/MOT17-12-FRCNN/img1',
+            '/home/frolov/U/MOTC/MOT17/test/MOT17-14-FRCNN/img1']
         
 for path in all_dirs:
     print("Now flowing " + dir)
-    command = './flow --imgdir ' + path + ' --model cfg/yolo.cfg --load bin/yolo.weights'
+    command = './flow --imgdir ' + path + ' --model cfg/yolo.cfg --load -1'
     command = command.split()
     cliHandler(command)
