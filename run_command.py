5e47a094ee648a9305c690a1ee0e74903ad49d13
Stanislav Frolov <frolov@mrtknecht2.mrt.uni-karlsruhe.de>
2017-08-30 16:47:34 +0200

    batch 2 seq 6

--- run_command.py
+++ run_command.py
@@ -25,7 +25,7 @@ Training Commands
 """
 Training on Machine
 """
-command = './flow --model cfg/full_motnet.cfg --train --load bin/yolo.weights --gpu 0.9 --batch 1 --seq_length 1'
+command = './flow --model cfg/full_motnet.cfg --train --load bin/yolo.weights --gpu 0.9 --batch 2 --seq_length 6'
 
 """
 Run the command
