47c60ae1a1a10413afb59cb0f51e4425018d744f
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2017-11-07 10:39:33 +0100

    continue train

--- run_command.py
+++ run_command.py
@@ -27,7 +27,7 @@ Training Commands
 """
 Training on Machine
 """
-command = './flow --model cfg/full_motnet.cfg --train --load bin/yolo.weights --gpu 0.9 --batch 16 --seq_length 1'
+command = './flow --model cfg/full_motnet.cfg --train --load -1 --gpu 0.9 --batch 16 --seq_length 1'
 
 """
 Run the command
