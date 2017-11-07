d27048dd403c82eb8a3867aa42d4e6c8ef73a672
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2017-11-07 10:24:50 +0100

    continue train higher lr

--- darkflow/net/help.py
+++ darkflow/net/help.py
@@ -58,7 +58,7 @@ def learning_rate(global_step, self):
   self.FLAGS.learningRate = tf.train.exponential_decay(
     self.FLAGS.lr, # Base learning rate.
     global_step,
-    1000, # Decay step.
+    7500, # Decay step.
     0.9, # Decay rate.
     staircase=True)
   return self.FLAGS.learningRate
